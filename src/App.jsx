9d3a6a25233229528f869f5bafe242665d349132
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-14 11:08:44 +0530

    Heading1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-
-import './App.css'
-import { motion } from "motion/react"
+import React from 'react'
 
 function App() {
-
   return (
-    <>
-       <motion.div
-            style={box}
-            animate={{ rotate: 360 }}
-            transition={{ duration: 1 }}
-        />
-       
-    </>
+    <div>
+      Framer 
+    </div>
   )
 }
 
