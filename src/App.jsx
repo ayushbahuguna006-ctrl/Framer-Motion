b1831999b8fd84a35a5738f7c9cefb0e5dfe96c1
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-16 15:58:19 +0530

    hover animations learnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { anticipate, motion } from "motion/react"
+import { motion } from "motion/react"
 import "./App.css"
 
 
 function App() {
   return (
     <div >
-     <motion.div className='bg-red-500 w-[20vh] m-2 h-[20vh]' initial={{x:0,y:0}} animate={{x:[0,1000,1000,0,0],y:[0,0,400,400,0]}} transition={{duration:4,delay:2,repeat:Infinity,}}>
+     <motion.div whileHover={{backgroundColor:"green"}} className='bg-red-500 w-[20vh] m-2 h-[20vh]' initial={{x:0,y:0}} animate={{x:[0,1000,1000,0,0],y:[0,0,400,400,0]}} transition={{duration:4,delay:2,repeat:1,ease:"anticipate"}}>
              
 
 
