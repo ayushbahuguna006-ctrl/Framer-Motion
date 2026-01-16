eb5e37dadde8bcc374ddbb3e79bc27e1861de6ab
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-16 16:21:48 +0530

    WhileTap gestures learnt

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css"
 function App() {
   return (
     <div >
-     <motion.div whileHover={{backgroundColor:"green"}} className='bg-red-500 w-[20vh] m-2 h-[20vh]' initial={{x:0,y:0}} animate={{x:[0,1000,1000,0,0],y:[0,0,400,400,0]}} transition={{duration:4,delay:1,repeat:1,ease:"anticipate"}}>
+     <motion.div whileHover={{backgroundColor:"green"}}  className='bg-red-500 w-[20vh] m-2 h-[20vh]' initial={{x:0,y:0}}  whileTap={{scale:0.5}}>
              
 
 
